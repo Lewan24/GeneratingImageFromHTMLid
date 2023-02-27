25b268b53c29fb16e56aa7721c7aa0aa59e6c368
Artur Lewandowski <LewanMordor@protonmail.com>
2023-02-27 14:30:01 +0100

    Dodaj pliki projektów.

---
+++ GeneratingImageFromHTML/wwwroot/js/jqueryfunctions.js
@@ -0,0 +1,10 @@
+﻿function GenerateImg(mydiv) {
+    html2canvas(document.getElementById(mydiv)).then(function (canvas) {
+        var anchorTag = document.createElement("a");
+        document.body.appendChild(anchorTag);
+        anchorTag.download = "generatedJPG.jpg";
+        anchorTag.href = canvas.toDataURL();
+        anchorTag.target = '_blank';
+        anchorTag.click();
+    });
+}
